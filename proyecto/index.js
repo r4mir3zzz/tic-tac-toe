4ccc06146320ea774090a091ebf16b68e624c378
r4mir3zzz <lramireza616@gmail.com>
2023-10-22 12:37:21 -0600

    Pasos en el js

--- proyecto/index.js
+++ proyecto/index.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo');
+/* Pasos para el JS
+*   Crear las variables de los jugadores x/o
+*   Crear las condiciones de victoria los casos posibles por tablero
+*   Crer constantes llamando a los elementos creados en el html como : - Mensaje de vistoria - Tablero -Botones de inicio y reinicio
+*   Crear la funcion con la que se inicio el juego
+*   Crear una funcion sobre lo que ocurre en el table -Empate -Victoria -Truno
+*   Funcion para el fin del juego si es un empate o alguno de los jugadores gana
+*   Funcion para intercambiar turnos
+*
+*   SE DEBE DEFINIR LOS ELEMENTOS DEL HTML Y CREARLOS
+*
+* */
